6477dc1e3c61d75eed733368b7e3d36db03e6619
Carson Johnson <carson74johnson@gmail.com>
2021-12-19 14:32:37 -0500

    added queries to resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,3 +2,23 @@ const { AuthenticationError } = require('apollo-server-express');
 const { Streamer, Comment } = require('../models/index');
 const { signToken } = require('../utils/index');
 
+const resolvers = {
+    Query: {
+        streamer: async (parent, { _id }) => {
+            const params = _id ? { _id } : {};
+            return Streamer.findOne(params).populate('createdComments');
+        },
+        comment: async (parent, { _id }) => {
+            const params = _id ? { _id } : {};
+            return await Comment.findById(params)
+                .populate('author')
+                .populate('body');
+        },
+        me: async (parent, args, context) => {
+            if (context.streamer) {
+                return Streamer.findOne({ _id: context.streamer._id }).populate('createdComments').populate({ path: 'createdComments' });
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    },
+}
