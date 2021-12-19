64bd3008acc4b7e4e8de2beb78bd452f23b9c588
Carson Johnson <carson74johnson@gmail.com>
2021-12-19 12:18:33 -0500

    started header

---
+++ client/src/components/PageComponents/Header.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import auth from '../../utils/auth';
+
+const Header = () => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Header;
