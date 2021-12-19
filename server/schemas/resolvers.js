81046d4a31128a27c8f89e9c78598c2f4023fc30
Carson Johnson <carson74johnson@gmail.com>
2021-12-19 14:41:30 -0500

    added mutaitons to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -21,4 +21,42 @@ const resolvers = {
             throw new AuthenticationError('You need to be logged in!');
         },
     },
+    Mutation: {
+        newStreamer: async (parent, args) => {
+            const streamer = await Streamer.create(args);
+            const token = signToken(streamer);
+            return { token, streamer };
+        },
+        login: async (parent, { streamername, password }) => {
+            const streamer = await Streamer.findOne({ streamername });
+
+            if (!streamer) {
+                throw new AuthenticationError('No user with this streamername found!');
+            }
+
+            const correctPw = await streamer.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password!');
+            }
+
+            const token = signToken(streamer);
+            return { token, streamer };
+        },
+        createComment: async (parent, args, context) => {
+            if (!context.streamer) {
+                throw new AuthenticationError('You need to be logged in to chat (create a comment).');
+            }
+
+            const createComment = async () => {
+                const comment = await Comment.create({ ...args, createdBy: context.user._id });
+                let streamerDocument = await Streamer.findOne({ _id: context.user._id });
+                streamerDocument.createdComments.push(comment._id);
+                await streamerDocument.save();
+                return comment;
+            };
+
+            return await createComment();
+        }
+    }
 }
