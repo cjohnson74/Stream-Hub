0b9ade2c4dd1ad728da9f3e9e4663f9bd3d40b50
Carson Johnson <carson74johnson@gmail.com>
2021-12-18 21:00:54 -0500

    added apollo server to server.js

--- server/server.js
+++ server/server.js
@@ -5,3 +5,13 @@ const path = require('path');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas/index.js');
 const { authMiddleware } = require('./utils/index.js');
+
+console.log('This is the server file');
+
+const server = new ApolloServer(
+    {
+        typeDefs,
+        resolvers,
+        context: authMiddleware
+    }
+)
