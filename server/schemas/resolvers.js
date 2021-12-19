e735196588c4b30e9144050100952fb0c323548c
Carson Johnson <carson74johnson@gmail.com>
2021-12-19 15:18:29 -0500

    added to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,12 +8,28 @@ const resolvers = {
             const params = _id ? { _id } : {};
             return Streamer.findOne(params).populate('createdComments');
         },
-        comment: async (parent, { _id }) => {
+        video: async (parent, { _id }) => {
             const params = _id ? { _id } : {};
-            return await Comment.findById(params)
-                .populate('author')
-                .populate('body');
+            return await Video.findById(params)
+                .populate('streamingService')
+                .populate('comments')
+                .populate({path: 'comments', populate:{ path: 'author'}});
         },
+        videos: async () => {
+            return await Video.find({}).populate('streamingService').sort({watchers: -1});
+        },
+        authVideosReq: async (parent, args, context) => {
+            if(!context.streamer){
+                throw new AuthenticationError('You need to be logged in to request these Videos')
+            }
+            return await Video.find({});
+        },
+        // comment: async (parent, { _id }) => {
+        //     const params = _id ? { _id } : {};
+        //     return await Comment.findById(params)
+        //         .populate('author')
+        //         .populate('body');
+        // },
         me: async (parent, args, context) => {
             if (context.streamer) {
                 return Streamer.findOne({ _id: context.streamer._id }).populate('createdComments').populate({ path: 'createdComments' });
@@ -43,13 +59,14 @@ const resolvers = {
             const token = signToken(streamer);
             return { token, streamer };
         },
+
         createComment: async (parent, args, context) => {
             if (!context.streamer) {
                 throw new AuthenticationError('You need to be logged in to chat (create a comment).');
             }
 
             const createComment = async () => {
-                const comment = await Comment.create({ ...args, createdBy: context.user._id });
+                const comment = await Comment.create({ ...args, author: context.user._id });
                 let streamerDocument = await Streamer.findOne({ _id: context.user._id });
                 streamerDocument.createdComments.push(comment._id);
                 await streamerDocument.save();
