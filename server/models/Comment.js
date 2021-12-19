717c2016804d1d32757f609487260342406f036a
Carson Johnson <carson74johnson@gmail.com>
2021-12-19 15:05:24 -0500

    added to comment model

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,23 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema(
-    {
-        author: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Streamer',
-        },
-        title: {
-            type: String,
-        },
-        body: {
-            type: String,
-            required: true,
-        },
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Streamer",
     },
-    {
-        timestamps: true,
-    }
+    title: {
+      type: String,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    thread: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const Comment = mongoose.model("Comment", CommentSchema);
