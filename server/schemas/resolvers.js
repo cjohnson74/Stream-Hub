7a8b1751fa2661ab0d76869f13d2f06485c212b3
Carson Johnson <carson74johnson@gmail.com>
2021-12-19 12:48:37 -0500

    finished type def models. Need to add queries and mutations

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,20 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Streamer {
+        _id: ID
+        streamername: String
+        email: String
+        firstName: String
+        lastName: String
+        image: String
+    }
+
+    type Comment {
+        author: Streamer
+        title: String
+        body: String!
+    }
+`;
+
+module.exports = typeDefs;
