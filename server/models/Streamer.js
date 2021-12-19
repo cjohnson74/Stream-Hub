ed82efc8e6a116f75c02665a47dfa3deb557c0b8
Carson Johnson <carson74johnson@gmail.com>
2021-12-19 12:42:00 -0500

    added Streamer.js schema and password salt is implemented

---
+++ server/models/Streamer.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema;
+
+const StreamerSchema = new Schema(
+    {
+        streamername: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        password: {
+            type: String,
+        },
+        email: {
+            type: String,
+            match: [/.+@.+\..+/, "Invalid, enter an e-mail address"],
+        },
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+        },
+        description: {
+            type: String,
+        },
+        image: {
+            type: String,
+            default: 'https://pngset.com/images/red-play-button-sphere-purple-triangle-balloon-transparent-png-1586266.png'
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+StreamerSchema.pre('save', function(next) {
+    if(this.password) {
+        let salt = bcrypt.genSaltSync(10)
+        this.password = bcrypt.hashSync(this.password, salt)
+        next()
+    }
+});
